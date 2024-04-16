95282b0644db19a513cd69f27da7243165edf971
jki5410 <166671365+jki5410@users.noreply.github.com>
2024-04-16 15:30:36 +0900

    make oil price DB from API

--- pro1/polls/models.py
+++ pro1/polls/models.py
@@ -8,3 +8,8 @@ class ExchangeRate(models.Model):
 
     def __str__(self):
         return self.cur_unit
+
+class OilPrice(models.Model):
+    date = models.DateField()
+    oil_category = models.CharField(max_length=100)
+    avg_price = models.DecimalField(max_digits=10, decimal_places=2)
