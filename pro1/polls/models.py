f77042287ed62160228ce0f3ed9adaa774a487be
juyounglee <james289@naver.com>
2024-04-16 12:19:28 +0900

    make instance from openapi

--- pro1/polls/models.py
+++ pro1/polls/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class ExchangeRate(models.Model):
-    curr_unit = models.CharField(max_length=10)
+    cur_unit = models.CharField(max_length=10)
     deal_bas_r = models.FloatField()
     search_date = models.DateTimeField()
 
     def __str__(self):
-        return self.curr_unit
+        return self.cur_unit
