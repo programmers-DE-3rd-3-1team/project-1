cbdf17a351c86558b34a045f90a4afc8ec5987a1
bongHsss <dev_bhs@naver.com>
2024-04-17 21:24:04 +0900

    USD.py Update

--- pro1/polls/data_graphs/USD.py
+++ pro1/polls/data_graphs/USD.py
@@ -1,68 +1,48 @@
 import sqlite3
 from matplotlib import pyplot as plt
+import pandas as pd
+import plotly.express as px
 
 # 데이터 연결해서 가져오기
-
 con = sqlite3.connect("db.sqlite3")
 Cur = con.cursor()
 
-Cur.execute("select * from polls_exchangerate where cur_unit = 'USD' order by 4;") 
+Cur.execute("select * from polls_exchangerate where cur_unit = 'USD' order by 4;") # 달러 4번째 컬럼이 날짜
 
 data = Cur.fetchall()
 con.close()
 
-price = []
-date = []
+USD_price = []
+USD_date = []
 
 for i in range(len(data)):
     # 가격 저장
-    price.append(data[i][2])
+    USD_price.append(data[i][1])
     
-    # 날짜 뒤에 시간 빼고 연도-월-일 으로 저장
-    a = data[i][3]
-    b = a.split(' ')
-    date.append(b[0])
+    # 날짜 저장
+    USD_date.append(data[i][3])
     
 print(data[:10])
 
-# 그래프 그리는 코드
-# plt.figure(figsize=(8, 3))
-# plt.plot(date, price, marker='o', linestyle='-')
-# plt.title('USD price by date')
-# plt.xticks(rotation=90)
-# plt.xlabel('Date')
-# plt.ylabel('USD Price')
-# plt.grid(True)
-# plt.show()
-
-# 저장
-# plt.savefig('my_plot.png')
-
-# 그림 저장은 제가 일단 파일에 올려두었습니다 서버 화면에 올라가도록 설정은 해두었으나 디자인을 좀 변경할 필요가 있어보입니다. 
 
 
-# 이 아래 주석은 반응형 그래프 적용 해보려고 하는 중 입니다!
-# import plotly.io as pio
-# fig = dict({ #plotly를 사용하는 첫번쨰 방법 - dict
-#     "data": [{"type": "line",
-#               "x": date,
-#               "y": price}],
-#     "layout": {"title": {"text": "딕셔너리로 그린 그래프"}} # 제목을 제시하려면?
-# })
-# pio.show(fig) #모아보여주기
 
-# print(type(date[1]), type(price[1]))
-
-# import pandas as pd
-
-# Daily_USD = pd.DataFrame({'price': price,
-#                             'date':date})
+Daily_USD = pd.DataFrame({'price': USD_price,
+                            'date': USD_date})
 # print(Daily_USD)
 
-# import plotly.express as px
-# # ImportError: Plotly express requires pandas to be installed. >> 오류 발생 시 pip install pandas
-# fig = px.line(Daily_USD,
-#               x="date",
-#               y="price",
-#               title='Life expectancy in Asia')
+# ImportError: Plotly express requires pandas to be installed. >> 오류 발생 시 pip install pandas
+fig = px.line(Daily_USD,
+              x="date",
+              y="price",
+              title='Daily_USD')
 # fig.show()
+## 레이아웃 업데이트
+fig.update_layout(
+    plot_bgcolor='white',  # 배경색을 하얀색으로 설정
+    title_font_size=24,    # 제목의 폰트 크기 설정
+    xaxis=dict(title='Date', showgrid=True),  # x축 레이블 설정
+    yaxis=dict(title='Price', showgrid=False),
+)
+
+fig.write_html("Daily_USD.html")
