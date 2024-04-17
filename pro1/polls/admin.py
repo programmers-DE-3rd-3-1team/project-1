2f761c819661474d90bb905bbee8fab420989698
bongHsss <dev_bhs@naver.com>
2024-04-17 18:20:35 +0900

    migrate solution good and graph complete

--- pro1/polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(ExchangeRate)
-admin.site.register(OilPrice)
-admin.site.register(GoldPrice)
-admin.site.register(Kospi)
-admin.site.register(WTIOilPrice)
