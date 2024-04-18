f14caffe97e0fcae6ced980b96bbfca266d32c94
jeonchansu <bingryun@gmail.com>
2024-04-18 16:12:01 +0900

    선점도 html 수정

--- pro1/polls/data_graphs/gold_usd.py
+++ pro1/polls/data_graphs/gold_usd.py
@@ -7,7 +7,7 @@ import plotly.express as px
 con = sqlite3.connect("db.sqlite3")
 Cur = con.cursor()
 
-Cur.execute("select a.rate, a.date from polls_exchangerate a inner join polls_kospi b on a.date = b.date order by a.date;") # 달러 4번째 컬럼이 날짜
+Cur.execute("select a.rate, a.date from polls_exchangerate a inner join polls_goldprice b on a.date = b.date order by a.date;") 
 
 data = Cur.fetchall()
 con.close()
@@ -28,39 +28,39 @@ for i in range(len(data)):
 con = sqlite3.connect("db.sqlite3")
 Cur = con.cursor()
 
-Cur.execute("select a.closing_price, a.date from polls_kospi a inner join polls_exchangerate b on a.date = b.date order by a.date;") # 금값 2번째 컬럼이 날짜
+Cur.execute("select a.closing_price, a.date from polls_goldprice a inner join polls_exchangerate b on a.date = b.date order by a.date;") 
 
 data = Cur.fetchall()
 con.close()
 
 # print(data[:10])
 
-kospi_price = []
-kospi_date = []
+gold_price = []
+gold_date = []
 
 for i in range(len(data)):
     # 가격 저장
-    kospi_price.append(data[i][0])
+    gold_price.append(data[i][0])
     
     # 날짜 뒤에 시간 빼고 연도-월-일 으로 저장
-    kospi_date.append(data[i][1])
+    gold_date.append(data[i][1])
 
 
-usd_kospi = pd.DataFrame({'usd_data': USD_price
-                        ,'kospi_data': kospi_price})
+usd_gold = pd.DataFrame({'usd_data': USD_price
+                        ,'gold_data': gold_price})
 
 # 상관계수 출력
 #print(usd_gold.corr(method='pearson'))
 
-fig = px.scatter(usd_kospi, x='usd_data', y='kospi_data', title='USD-KOSPI', size_max=1)
+fig = px.scatter(usd_gold, x='gold_data', y='usd_data', title='GOLD-USD', size_max=1)
 
 # ImportError: Plotly express requires pandas to be installed. >> 오류 발생 시 pip install pandas
 # 레이아웃 업데이트
 fig.update_layout(
     plot_bgcolor='white',  # 배경색을 하얀색으로 설정
     # title_font_size=24,    # 제목의 폰트 크기 설정
-    xaxis=dict(title='USD', showgrid=True),  # x축 레이블 설정
-    yaxis=dict(title='KOSPI', showgrid=False),
+    xaxis=dict(title='GOLD', showgrid=True),  # x축 레이블 설정
+    yaxis=dict(title='USD', showgrid=False),
 )
 
-fig.write_html("usd_kospi.html")
+fig.write_html("gold_usd.html")
